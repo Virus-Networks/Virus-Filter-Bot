2a5933062072cd3eaeac6edcd8c1accfed3c5fa3
Virus-Networks <168676321+Virus-Networks@users.noreply.github.com>
2024-05-26 20:49:46 +0530

    Update info.py

--- info.py
+++ info.py
@@ -59,8 +59,8 @@ VERIFY = bool(environ.get('VERIFY', False)) # Verification On ( True ) / Off ( F
 HOWTOVERIFY = environ.get('HOWTOVERIFY', 'https://t.me/Virus_Botz/34') # How to open tutorial link for verification
 
 # Others
-SHORTLINK_URL = environ.get('SHORTLINK_URL', 'krishnalink.com')
-SHORTLINK_API = environ.get('SHORTLINK_API', '1494a54146906d90579ad637308b265b3c159849')
+SHORTLINK_URL = environ.get('SHORTLINK_URL', 'instantearn.in/')
+SHORTLINK_API = environ.get('SHORTLINK_API', '978b5b5fa87c70ed882fbf9243f5cb67218a4b1a')
 IS_SHORTLINK = bool(environ.get('IS_SHORTLINK', True))
 DELETE_CHANNELS = [int(dch) if id_pattern.search(dch) else dch for dch in environ.get('DELETE_CHANNELS', '0').split()]
 MAX_B_TN = environ.get("MAX_B_TN", "10")
