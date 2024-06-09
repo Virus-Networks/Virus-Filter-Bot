167a82d20a8215dcbd2af553605ccad1b0c45557
Virus-Networks <168676321+Virus-Networks@users.noreply.github.com>
2024-06-09 19:57:24 +0530

    Update info.py

--- info.py
+++ info.py
@@ -104,7 +104,7 @@ if 'DYNO' in environ:
     APP_NAME = environ.get('APP_NAME')
 else:
     ON_HEROKU = False
-BIND_ADRESS = str(getenv('WEB_SERVER_BIND_ADDRESS', 'https://{}/'))
+BIND_ADRESS = str(getenv('WEB_SERVER_BIND_ADDRESS', 'https://cinexheavenbymrvirus02-5360ca72ce5e.herokuapp.com/'))
 FQDN = str(getenv('FQDN', BIND_ADRESS)) if not ON_HEROKU or getenv('FQDN') else APP_NAME+'.herokuapp.com'
 URL = "https://{}/".format(FQDN) if ON_HEROKU or NO_PORT else \
     "https://{}/".format(FQDN, PORT)
@@ -122,9 +122,9 @@ else:
     ON_HEROKU = False
 HAS_SSL=bool(getenv('HAS_SSL',True))
 if HAS_SSL:
-    URL = "https://filterbot-04b426038ed4.herokuapp.com/".format(FQDN)
+    URL = "https://cinexheavenbymrvirus02-5360ca72ce5e.herokuapp.com/".format(FQDN)
 else:
-    URL = "https://filterbot-04b426038ed4.herokuapp.com/".format(FQDN)
+    URL = "https://cinexheavenbymrvirus02-5360ca72ce5e.herokuapp.com/".format(FQDN)
 
 # add premium logs channel id
 PREMIUM_LOGS = int(environ.get('PREMIUM_LOGS', '-1002038773406'))
